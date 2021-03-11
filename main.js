a82072fe5a792cfc9ce3c5ee6c50c19b5e98d958
Giorgi Lomaia <giorgilomaia1710@gmail.com>
2021-03-11 21:20:39 +0400

    done

---
+++ main.js
@@ -0,0 +1,25 @@
+//First task
+var x = 3
+var y = -4 
+var sum = x + y
+if(sum%2===0 && sum>0){
+  console.log('Even')
+}
+else if(sum<0){
+  console.log('The sum is negative')
+}
+else{
+  console.log('Odd')
+}
+
+//Second
+
+var b = 5
+var h = 2
+
+b=Math.abs(b)
+h=Math.abs(h)
+
+var area=(b*h)/2 
+
+console.log(area)
