a7340e56bb281a14343e0a880d1b11933e23cd62
Giorgi Lomaia <giorgilomaia1710@gmail.com>
2021-03-24 20:02:31 +0400

    js

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
-//First task
-console.log("First Task")
-var x = 3
-var y = -4 
-var sum = x + y
-if(sum%2===0){
-  console.log('Even')
+// Adds variables to DOM
+var itemInput = document.getElementById('item-input')
+var priceInput = document.getElementById('price-input')
+var addProductBtn = document.getElementById('add-product')
+var productList = document.getElementById('product-list')
+var priceList = document.getElementById('price-list')
+var priceBtn = document.getElementById('price')
+
+// creates arrays for list
+
+var products = []
+var price = []
+
+// adds products to the list 
+
+var renderAllItem = function() {
+  productList.innerText = ''
+  priceList.innerText = ''
+  products.forEach(function(list){
+    var itemLi = document.createElement('li')
+    itemLi.innerText = list 
+    productList.appendChild(itemLi)
+  })
+  
+  // adds prices to the list
+  
+  price.forEach(function(list){
+    var itemLi = document.createElement('li')
+    itemLi.innerText = list + '₾'
+    priceList.appendChild(itemLi)
+  })
+  itemInput.value = ''
+  priceInput.value = ''
 }
-else{
-  console.log('Odd')
+
+// creates functions for buttons
+
+// gets input values and sends to arr
+var handleButtonClick = function(){
+  var itemValue = itemInput.value
+  var priceValue = Number(priceInput.value)
+  products.push(itemValue)
+  price.push(priceValue)
+  renderAllItem()
 }
 
-//Second
-console.log("Second task")
-var b = 5
-var h = 2
+// sum function
+var sumBtn = function(){
+  var sum = price.reduce(function(a,b){
+    return a + b 
+  }, 0)
+  document.getElementById("sum").innerHTML = sum+'₾'
+  
+}
+// adds functions to buttons
 
-b=Math.abs(b)
-h=Math.abs(h)
-
-var area=(b*h)/2 
-
-console.log(area)
+addProductBtn.addEventListener('click', handleButtonClick)
+priceBtn.addEventListener('click', sumBtn)
